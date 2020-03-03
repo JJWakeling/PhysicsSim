73a939ab0dddd9cc19d19a04297a4c74301bc26b
Joseph Wakeling <josephjwakeling@gmail.com>
2020-03-03 12:26:46 +0000

    remove unnecessary default references

--- Controller/IObserver.cs
+++ Controller/IObserver.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Console;
+using Model;
+
 namespace Controller
 {
     public interface IObserver
@@ -9,10 +11,10 @@ namespace Controller
     public class FakeObserver : IObserver
     {
         private readonly double width, height;
-        private readonly Model.IDensityModel model;
-        private readonly Console.IScreen screen;
+        private readonly IDensityModel model;
+        private readonly IScreen screen;
 
-        public FakeObserver(double width, double height, Console.IScreen screen, Model.IDensityModel model)
+        public FakeObserver(double width, double height, IScreen screen, IDensityModel model)
         {
             this.width = width;
             this.height = height;
