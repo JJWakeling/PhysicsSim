750861dae278de52a1dce7493e640f234f00b877
Joseph Wakeling <josephjwakeling@gmail.com>
2020-03-04 19:49:24 +0000

    implement frame refreshing

--- ControllerImplementation/FixedRectangleObserver.cs
+++ ControllerImplementation/FixedRectangleObserver.cs
@@ -1,6 +1,7 @@
 ﻿using Controller;
 using Console;
 using Model;
+using System.Timers;
 
 namespace ControllerImplementation
 {
@@ -9,15 +10,41 @@ namespace ControllerImplementation
         private readonly IDensityModel model;
         private readonly IScreen screen;
         private readonly IRectangle viewBoundary;
+        private readonly int frameDuration;
+        private readonly double modelInterval;
+        private double currentTime;
 
-        public FixedRectangleObserver(IScreen screen, IDensityModel model, IRectangle viewBoundary)
+        public FixedRectangleObserver(IScreen screen, IDensityModel model, IRectangle viewBoundary, int frameDuration, double modelInterval)
         {
             this.model = model;
             this.screen = screen;
             this.viewBoundary = viewBoundary;
+            this.frameDuration = frameDuration;
+            this.modelInterval = modelInterval;
+
+            currentTime = 0.0d;
         }
 
         public void Run()
+        {
+            Timer frameTimer = new Timer(frameDuration);
+            frameTimer.Elapsed += FrameTimer_Elapsed;
+            frameTimer.AutoReset = true;
+            frameTimer.Enabled = true;
+        }
+
+        private void FrameTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            RefreshScreen();
+            Tick();
+        }
+
+        private void Tick()
+        {
+            currentTime += modelInterval;
+        }
+
+        private void RefreshScreen()
         {
             IRectangle[] rows = viewBoundary.WidthwisePartition(screen.Width());
             for (int i = 0; i < rows.Length; i++)
@@ -25,7 +52,7 @@ namespace ControllerImplementation
                 IRectangle[] pixels = rows[i].HeightwisePartition(screen.Height());
                 for (int j = 0; j < pixels.Length; j++)
                 {
-                    screen.OverwritePixel(i, j, model.Density(0, pixels[j]));
+                    screen.OverwritePixel(i, j, model.Density(currentTime, pixels[j]));
                 }
             }
 
--- PhysicsSim/Program.cs
+++ PhysicsSim/Program.cs
@@ -15,9 +15,10 @@ namespace PhysicsSim
             IDensityCharacterMap characterMap = new FakeDensityCharacterMap();
             IScreen screen = new FixedSizeWindowsConsole(50, 50, characterMap);
             IRectangle viewBoundary = new Rectangle(0.0d, 0.0d, 50.0d, 50.0d);
-            IObserver observer = new FixedRectangleObserver(screen, model, viewBoundary);
+            IObserver observer = new FixedRectangleObserver(screen, model, viewBoundary, 1000, 1.0d);
 
             observer.Run();
+            System.Console.ReadLine();
         }
     }
 }
