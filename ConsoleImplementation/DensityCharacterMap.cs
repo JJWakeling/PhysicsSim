39016088b91cd1bb43a7f60e013c1a8b9746978b
Joseph Wakeling <josephjwakeling@gmail.com>
2020-03-29 17:11:41 +0100

    write template for DensityCharacterMap class

---
+++ ConsoleImplementation/DensityCharacterMap.cs
@@ -0,0 +1,20 @@
+﻿using Console;
+
+namespace ConsoleImplementation
+{
+    class DensityCharacterMap : IDensityCharacterMap
+    {
+        private readonly double[] thresholds;
+        private readonly char[] outputs;
+
+        public DensityCharacterMap(double[] thresholds, char[] outputs)
+        {
+
+        }
+
+        public char Character(double density)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
