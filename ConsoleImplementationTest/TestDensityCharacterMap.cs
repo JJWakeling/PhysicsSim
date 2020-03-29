02e5fee89d450313d1fc7c7eef54f276a055eb9a
Joseph Wakeling <josephjwakeling@gmail.com>
2020-03-29 17:23:24 +0100

    template tests for DensityCharacterMap

---
+++ ConsoleImplementationTest/TestDensityCharacterMap.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleImplementation;
+using NUnit.Framework;
+
+namespace ConsoleImplementationTest
+{
+    [TestFixture]
+    public class TestDensityCharacterMap
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test_constructor_errors_on_outputs_too_short()
+        {
+
+        }
+
+        [Test]
+        public void Test_constructor_errors_on_outputs_too_long()
+        {
+
+        }
+
+        [Test]
+        public void Test_Character_returns_right_value_below_lowest_threshold()
+        {
+
+        }
+
+        [Test]
+        public void Test_Character_returns_right_value_above_highest_threshold()
+        {
+
+        }
+
+        [Test]
+        public void Test_Character_returns_right_value_between_thresholds()
+        {
+
+        }
+    }
+}
